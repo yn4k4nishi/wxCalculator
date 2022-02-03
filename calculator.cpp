4ac962ca366c0fd167813b3204886860d1867e4b
yn4k4ishi <yn4k4nishi.kit@gmail.com>
2022-02-04 01:06:17 +0900

    :sparkles: Feature :

---
+++ calculator.cpp
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "noname.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+MyFrame4::MyFrame4( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+
+	wxFlexGridSizer* fgSizer3;
+	fgSizer3 = new wxFlexGridSizer( 0, 1, 0, 0 );
+	fgSizer3->SetFlexibleDirection( wxBOTH );
+	fgSizer3->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+
+	wxStaticBoxSizer* sbSizer6;
+	sbSizer6 = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, wxT("display") ), wxVERTICAL );
+
+	sbSizer6->SetMinSize( wxSize( 300,-1 ) );
+	m_textCtrl2 = new wxTextCtrl( sbSizer6->GetStaticBox(), wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+	sbSizer6->Add( m_textCtrl2, 0, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALL, 5 );
+
+
+	fgSizer3->Add( sbSizer6, 0, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL, 5 );
+
+	wxStaticBoxSizer* sbSizer8;
+	sbSizer8 = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, wxT("label") ), wxHORIZONTAL );
+
+	m_button43 = new wxButton( sbSizer8->GetStaticBox(), wxID_ANY, wxT("C"), wxDefaultPosition, wxDefaultSize, 0 );
+	sbSizer8->Add( m_button43, 0, wxALL, 5 );
+
+	m_button44 = new wxButton( sbSizer8->GetStaticBox(), wxID_ANY, wxT("AC"), wxDefaultPosition, wxDefaultSize, 0 );
+	sbSizer8->Add( m_button44, 0, wxALL, 5 );
+
+
+	fgSizer3->Add( sbSizer8, 1, wxALL|wxEXPAND, 5 );
+
+	wxStaticBoxSizer* sbSizer9;
+	sbSizer9 = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, wxT("label") ), wxVERTICAL );
+
+	wxGridSizer* gSizer3;
+	gSizer3 = new wxGridSizer( 4, 4, 0, 0 );
+
+	NUM7 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("7"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM7, 0, wxALL|wxEXPAND, 5 );
+
+	NUM8 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("8"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM8, 0, wxALL|wxEXPAND, 5 );
+
+	NUM9 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("9"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM9, 0, wxALL|wxEXPAND, 5 );
+
+	Div = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("/"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( Div, 0, wxALL|wxEXPAND, 5 );
+
+	NUM4 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("4"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM4, 0, wxALL|wxEXPAND, 5 );
+
+	NUM5 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("5"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM5, 0, wxALL|wxEXPAND, 5 );
+
+	NUM6 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("6"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM6, 0, wxALL|wxEXPAND, 5 );
+
+	Mult = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("x"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( Mult, 0, wxALL|wxEXPAND, 5 );
+
+	NUM1 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("1"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM1, 0, wxALL|wxEXPAND, 5 );
+
+	NUM2 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("2"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM2, 0, wxALL|wxEXPAND, 5 );
+
+	NUM3 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("3"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM3, 0, wxALL|wxEXPAND, 5 );
+
+	Sub = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("-"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( Sub, 0, wxALL|wxEXPAND, 5 );
+
+	NUM0 = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("0"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( NUM0, 0, wxALL|wxEXPAND, 5 );
+
+	Dot = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("."), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( Dot, 0, wxALL|wxEXPAND, 5 );
+
+	Equal = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("="), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer3->Add( Equal, 0, wxALL|wxEXPAND, 5 );
+
+	Add = new wxButton( sbSizer9->GetStaticBox(), wxID_ANY, wxT("+"), wxDefaultPosition, wxSize( -1,60 ), 0 );
+	gSizer3->Add( Add, 0, wxALL|wxEXPAND, 5 );
+
+
+	sbSizer9->Add( gSizer3, 1, wxEXPAND, 5 );
+
+
+	fgSizer3->Add( sbSizer9, 1, wxALL|wxEXPAND, 5 );
+
+
+	this->SetSizer( fgSizer3 );
+	this->Layout();
+
+	this->Centre( wxBOTH );
+}
+
+MyFrame4::~MyFrame4()
+{
+}
---
+++ calculator.h
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/string.h>
+#include <wx/textctrl.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/sizer.h>
+#include <wx/statbox.h>
+#include <wx/button.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/frame.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyFrame4
+///////////////////////////////////////////////////////////////////////////////
+class MyFrame4 : public wxFrame
+{
+	private:
+
+	protected:
+		wxTextCtrl* m_textCtrl2;
+		wxButton* m_button43;
+		wxButton* m_button44;
+		wxButton* NUM7;
+		wxButton* NUM8;
+		wxButton* NUM9;
+		wxButton* Div;
+		wxButton* NUM4;
+		wxButton* NUM5;
+		wxButton* NUM6;
+		wxButton* Mult;
+		wxButton* NUM1;
+		wxButton* NUM2;
+		wxButton* NUM3;
+		wxButton* Sub;
+		wxButton* NUM0;
+		wxButton* Dot;
+		wxButton* Equal;
+		wxButton* Add;
+
+	public:
+
+		MyFrame4( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 326,472 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+
+		~MyFrame4();
+
+};
+
